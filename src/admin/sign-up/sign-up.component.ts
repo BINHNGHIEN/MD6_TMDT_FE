3ab748866466c5736a0ef2af7062271d1b28bdeb
manhka <manh87835@gmail.com>
2022-07-20 15:49:51 +0700

    Done

--- src/admin/sign-up/sign-up.component.ts
+++ src/admin/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {AuthService} from "../../service/auth.service";
+import {Router} from "@angular/router";
+import {SignUpForm} from "../../model/SignUpForm";
 
 @Component({
   selector: 'app-sign-up',
@@ -9,35 +11,56 @@ import {AuthService} from "../../service/auth.service";
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+
+  status = 'Please fill in the form to register';
+  form: any = {};
+  hide = true;
   formSignUp = new FormGroup({
-    name: new FormControl(''),
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    name: new FormControl(),
+    email: new FormControl(),
+    username: new FormControl(),
+    password: new FormControl(),
+    role: new FormControl(''),
   })
 
-  constructor(private httpClient: HttpClient,
-              private authService: AuthService) {
+  constructor( private authService: AuthService,
+               private router: Router) { }
+
+  ngOnInit(): void{
   }
-
-  ngOnInit(): void {
-  }
-
-  obj: any;
-
+  // @ts-ignore
+  signUpForm: SignUpForm;
+  error1: any = {
+    message: 'no_user'
+  };
+  error2: any = {
+    message: 'no_email'
+  };
+  success: any = {
+    message: 'yes'
+  };
   createUser() {
-    console.log(this.formSignUp.value);
-    this.obj = {
-      name: this.formSignUp.value.name,
-      username: this.formSignUp.value.username,
-      email: this.formSignUp.value.email,
-      password: this.formSignUp.value.password
-    }
-    this.authService.signUp(this.obj).subscribe(() => {
-      alert('Thành công');
-    }, error => {
-      alert('Lỗi');
-    })
+    this.signUpForm = new SignUpForm(
+      this.form.name,
+      this.form.username,
+      this.form.email,
+      this.form.password,
+    );
+    console.log('vao mail chua  ====> ', this.form.email)
+    this.authService.signUp(this.signUpForm).subscribe(data => {
+      console.log(data)
+      if (JSON.stringify(data) == JSON.stringify(this.error1)){
+        this.status = 'username is existed! Please try again!';
+        alert('The username is existed! Please try again!')
+      }
+      if (JSON.stringify(data) == JSON.stringify(this.error2)){
+        this.status = 'The email is existed! Please try again!';
+        alert('The email is existed! Please try again!')
+      }
+      if (JSON.stringify(data) == JSON.stringify(this.success)){
+        this.status = 'Create account success!';
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
 }
